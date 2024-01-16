976830c396eeb58c88b312bb30cf4e7e340ca8d1
tingguoguoguo <tina60215@gmail.com>
2024-01-17 00:12:49 +0800

    section02 90%

--- js/all.js
+++ js/all.js
@@ -1,4 +1,4 @@
-/* 輪播圖 */
+/* 大輪播圖 */
 //初始化輪播圖
 var swiper = new Swiper(".mySwiper", {
   pagination: {
@@ -18,6 +18,7 @@ $(".swiper-pagination").on("click", ".swiper-pagination-bullet", function () {
   swiper.slideTo(index);
 });
 
+/* section02輪播圖 */
 var swiper2 = new Swiper(".mySwiper2", {
   slidesPerView: 2,
   spaceBetween: 30,
@@ -41,3 +42,41 @@ var swiper2 = new Swiper(".mySwiper2", {
   }
 
 });
+
+// 創建一個 MediaQueryList 物件，檢查視窗寬度是否在 md 尺寸以上
+var mediaQuery = window.matchMedia('(min-width: 768px)');
+
+/* section02 gsap動畫 */
+$('.swiper-slide').hover(
+  function () {
+    // 只在 md 尺寸以上時執行動畫
+    if (mediaQuery.matches) {
+      var info = $(this).find('.info');
+      gsap.to(info, {
+        left: 72,
+        duration: 0.3,
+        ease: 'power2.inOut',
+      });
+    }
+    // 獲取當前 swiper-slide 的 data-image 属性值
+    var newImageSrc = $(this).attr('data-image');
+
+    // 設置 img 元素的 src 屬性為新的圖片
+    $(this).find('img').attr('src', newImageSrc);
+  },
+  function () {
+    // 只在 md 尺寸以上時執行動畫
+    if (mediaQuery.matches) {
+      var info = $(this).find('.info');
+      gsap.to(info, {
+        left: -149,
+        duration: 0.3,
+        ease: 'power2.inOut',
+      });
+    }
+    // 恢復原本的img src
+    var originalImageSrc = $(this).find('img').data('original-src');
+    $(this).find('img').attr('src', originalImageSrc);
+  }
+);
+
